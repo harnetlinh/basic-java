89350a8a8ba6f8685e8026f0cca47feafea60bde
linhhn13 <linhhn13@fe.edu.vn>
2023-10-02 19:36:10 +0700

    homework

--- Buoi1/variableExercise.java
+++ Buoi1/variableExercise.java
@@ -6,6 +6,18 @@ public class variableExercise {
          * 2. Khai báo 2 biến c và d kiểu số thực, tính hiệu của 2 biến đó và in ra màn hình
          * 3. Khai báo 2 biến e và f kiểu số thực, phép tính 1/e + 1/f và in ra màn hình
          * 4. Khai báo 2 biến g và h kiểu số nguyên, phép tính g^2 + h^2 và in ra màn hình
+         * 5. Khai báo 2 biến i và j kiểu số nguyên, phép tính (1/2*i + 1/2*j) và in ra màn hình
+         * 6. Khai báo 2 biến k và l kiểu số nguyên, phép tính (k^2 + l^2)^(1/2) và in ra màn hình
+         * 7. In ra màn hình chu vi và diện tích hình tròn khi biết bán kính
+         * 8. In ra màn hình chu vi và diện tích hình chữ nhật khi biết chiều dài và chiều rộng
+         * 9. In ra màn hình kim tự tháp hình sao (ví dụ:
+         *         *
+         *        ***
+         *       *****
+         *      *******
+         *     *********
+         * 10. Nhập vào tên và tuổi, in ra màn hình "Xin chào <tên>, năm nay bạn <tuổi> tuổi"
+         *
          */
     }
 }
