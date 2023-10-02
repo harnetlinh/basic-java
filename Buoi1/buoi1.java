43c53db952c10c66e1080dc098e42eba4952c2b0
linhhn13 <linhhn13@fe.edu.vn>
2023-10-02 18:18:53 +0700

    buoi1

---
+++ Buoi1/buoi1.java
@@ -0,0 +1,16 @@
+/**
+ * buoi1
+ */
+public class buoi1 {
+    public static void main(String[] args) {
+        // code here
+        System.out.println("Hello World Linh"); // xuống dòng
+        System.out.print("Hello World "); // không xuống dòng
+        System.out.print("Linh"); // không xuống dòng
+        /**
+         * Compiler: chương trình dịch code java thành mã máy
+         * Compiler sẽ không chạy vào đây
+         * 
+         */
+    }
+}
---
+++ Buoi1/variable.java
@@ -0,0 +1,16 @@
+public class variable {
+    public static void main(String[] args) {
+        int a; // khai báo biến a kiểu số nguyên 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 22, 100
+        double b; // khai báo biến b kiểu số thực 1.2, 5.78, 9.0, 10.5
+        float e; // khai báo biến e kiểu số thực nhưng ít chính xác hơn double
+        char c; // khai báo biến c kiểu ký tự ('a', 'b', 'c', '1', '2', '@')
+        boolean d; // khai báo biến đúng sai (0 và 1 - false và true)
+
+        a = 5; // gán giá trị 5 cho biến a (dấu  = ở đây là dấu gán giá trị)
+        System.out.println(a); // in ra màn hình giá trị của biến a
+
+        String str = "Hello World"; // khai báo biến str kiểu chuỗi
+
+        System.out.println(str + " Linh"); // in ra màn hình giá trị của biến str
+    }
+}
